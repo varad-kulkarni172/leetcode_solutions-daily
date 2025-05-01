92b80b5b6ec43e7b1ced9f4e95f4ffb329021a34
Varad Kulkarni <varadkulkarni172@gmail.com>
2025-05-01 19:59:47 +0530

    Time: 3 ms (73.22%), Space: 43.5 MB (56%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,11 +1,56 @@
 public class Solution {
     public boolean isPalindrome(String s) {
-        StringBuilder newStr = new StringBuilder();
-        for (char c : s.toCharArray()) {
-            if (Character.isLetterOrDigit(c)) {
-                newStr.append(Character.toLowerCase(c));
-            }
+        // StringBuilder str = new StringBuilder(); // create a mutable string to modify
+        // for(char c : s.toCharArray()){
+        //     if(Character.isLetterOrDigit(c)){ // Wrap in Character utility to filter after converting it to alphanumeric
+        //         str.append(Character.toLowerCase(c));
+        //     }
+        // }
+        // return str.toString().equals(str.reverse().toString());
+
+    //     int l = 0, r = s.length() - 1;
+
+    //     while(l < r){
+    //         while(l < r && !alphanumeric(s.charAt(l))){
+    //             l++;
+    //         }
+    //         while(r > l && !alphanumeric(s.charAt(r))){
+    //             r--;
+    //         }
+
+    //         if(Character.toLowerCase(s.charAt(l)) != Character.toLowerCase(s.charAt(r))){
+    //             return false;
+    //         }
+
+    //         l++; r--;
+    //     }
+    //     return true;
+    // }
+
+    // public boolean alphanumeric(char c){
+    //     return (c >= 'A' && c <= 'Z' || 
+    //         c >= 'a' && c <= 'z' || 
+    //         c >= '0' && c <= '9');
+    //     }
+
+    // My own solution using stringbuilder, alphanumeric and 2 pointers; TC is O(n) and SC is also O(n)
+    StringBuilder newStr = new StringBuilder();
+    for(char c : s.toCharArray()){
+        if(Character.isLetterOrDigit(c)){
+            newStr.append(Character.toLowerCase(c));
         }
-        return newStr.toString().equals(newStr.reverse().toString());
+    }
+
+    String cleanStr = newStr.toString();
+
+    int l = 0, r = cleanStr.length() - 1;
+
+    while(l < r){
+        if(cleanStr.charAt(l) != cleanStr.charAt(r)){
+            return false;
+        }
+        l++; r--;
+    }
+    return true;
     }
 }
